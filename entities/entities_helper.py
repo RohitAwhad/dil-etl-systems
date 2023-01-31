b63c7ec2f86a9ff033855c38c29b088f12ac5325
RohitAwhad <39838903+RohitAwhad@users.noreply.github.com>
2023-01-31 17:51:28 +0530

    Delete entities directory

--- entities/entities_helper.py
+++
@@ -1,9 +0,0 @@
-from modules.utilities.aws.rds.tables.types.county_type import CountyType
-from modules.entities.county_helper import add_county_type_fk
-
-
-
-def get_custom_merge(engine, df, fk):
-    if fk.model == CountyType:
-        df = add_county_type_fk(engine, df, fk)
-    return df
--- entities/state_helper.py
+++
@@ -1,8 +0,0 @@
-
-from modules.utilities.aws.rds.create.row_inserter import load_table_from_df
-from modules.utilities.aws.rds.query.foreign_key_helper import add_fks
-
-
-def add_mean_assignments(engine, df, metric_config):
-    df_w_fks = add_fks(engine, df, metric_config.fks)
-    load_table_from_df(engine, metric_config.model, df_w_fks)
